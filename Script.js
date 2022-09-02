caf5893852fb5f2c23f28cb276c6fb3549964d42
Juliano-silva <88514570+Juliano-silva@users.noreply.github.com>
2022-09-02 18:03:41 -0300

    Hoje

--- Script.js
+++ Script.js
@@ -1,11 +1,8 @@
 const changeThemeBtn = document.querySelector("#change-theme");
-
-// Toggle dark mode
 function toggleDarkMode() {
   document.body.classList.toggle("dark");
 }
 
-// Load light or dark mode
 function loadTheme() {
   const darkMode = localStorage.getItem("dark");
 
